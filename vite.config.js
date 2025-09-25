5fceebcf40d74fd0f2dcd3c73097c567a7b20f7d
root <root@testwebsitepiastrellista.local>
2025-09-25 20:21:34 +0000

    Aggiorna vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, "../be/src/main/resources/static"),
+    outDir: path.resolve(__dirname, "../dist"),
     emptyOutDir: true,
   },
+  base: "/",
 });
