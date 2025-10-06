a562d33992e42d3d9f4db81f9c6b23c8a41a96ca
Riccardo Marchetti <marchettiriccardo04@gmail.com>
2025-10-06 21:59:01 +0200

    Modifiche vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path";
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: path.resolve(__dirname, "./public_html"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
   base: "/",
